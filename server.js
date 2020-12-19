3b805a27020313997de617c4e09e6a58b8194f81
Victor Mendizabal <2211hugo@gmail.com>
2020-12-19 10:05:21 -0600

    fixed route problems

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 // Dependecies
 const express = require("express");
-const path = path("path");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static("public"));
+console.log(path.join(__dirname, "index.html"));
+console.log(path.join(__dirname, "notes.html"));
 //Routes for AJAx calls
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"))
 });
 
+
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "notes.html"))
 });
+
+// app.get("/api/notes", function(req, res) {
+//     return res.json(notes);
+// })
 
 
 
